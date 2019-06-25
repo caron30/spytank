ef5cbbf529f4fae7b0d0c7a6926c3d167392c7ec
caron <caronmulongo30@gmail.com>
2019-06-25 13:55:39 +0200

    caron

---
+++ basic_remote/remote.py
@@ -0,0 +1,45 @@
+# z: avancer 
+# q : gauche
+# s : reculer
+# d : droite
+# e/r : led 1 ou 0 
+# a : stop  
+# c : exit
+
+import spytank
+import time 
+
+print("z: avancer \nq : gauche\ns:reculer\nd :d : droite\ne/r : led 1 ou 0\na : stop\nc : exit ")
+print("entre une lettre pour piloter le comme dans la description")
+
+continuer =  True
+while continuer: 
+        
+    lettre = input(">>")
+
+    vitesse= 200
+       
+    if lettre == "z" : 
+        spytank.avance(vitesse)
+    elif lettre == "q":
+        spytank.gauche(vitesse)
+    elif lettre =="s":
+        spytank.recule(vitesse)
+    elif lettre == "d":
+        spytank.droite(vitesse)
+    elif lettre =="e":
+        spytank.led(0,1)
+        spytank.led(2,1)
+        spytank.led(1,1)
+        spytank.led(3,1)
+    elif lettre == "r":
+        spytank.led(0,0)
+        spytank.led(1,0)
+        spytank.led(2,0)
+        spytank.led(3,0)
+    elif lettre == "a":
+        spytank.stop()
+
+    elif lettre =="c":
+        spytank.stop()
+        continuer = False
